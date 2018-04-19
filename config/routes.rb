c7c61095dcd0e2b4f6c7e0d7265fa36be92e2606
George Eddie <geddie.code@gmail.com>
2018-04-19 12:13:05 +0100

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   devise_for :users
+  resources :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "users#index"
   get '/users/:id/edit', to: 'users#edit', as: 'user_edit'
   put '/user/update/:id', to: 'users#update'
 end
+

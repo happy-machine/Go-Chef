8884e3d6f9a7c06a50b897de3469abf3d6be38c4
Happy Machine <thisisdjfresh@gmail.com>
2018-04-19 11:08:35 +0100

    pre ahmed day2

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users
   root "users#index"
   get '/users/:id/edit', to: 'users#edit', as: 'user_edit'
+  get '/test_mode', to: 'users#test', as: 'test_mode'
   put '/user/update/:id', to: 'users#update'
 end
